c6139192c8c72b5db9501d1895d01697d8c1b284
Zane Durkin <zanedurkin@gmail.com>
2025-05-10 07:02:27 +0000

    fix: fixing formating. Fixing husky pre-commit check

--- src/challenge-pack-schema/index.ts
+++ src/challenge-pack-schema/index.ts
@@ -1,39 +1,40 @@
 import { z } from "zod";
 
-export const ChallengePack = z.object({
-  challenges: z.array(
-    z.object({
-      id: z.string().meta({
-        description: "The unique identifier for the challenge bundle",
-        examples: ["challenge-1", "challenge-2"],
-      }),
-      slug: z
-        .string()
-        .optional()
-        .meta({
-          description:
-            "The short name for the challenge. Useful when the ID is a UUID.",
+export const ChallengePack = z
+  .object({
+    challenges: z.array(
+      z.object({
+        id: z.string().meta({
+          description: "The unique identifier for the challenge bundle",
           examples: ["challenge-1", "challenge-2"],
         }),
-      category: z.string().meta({
-        description: "The category of the challenge",
-        examples: ["web", "crypto", "forensics"],
-      }),
-      value: z.number().meta({
-        description: "The score/value of the challenge",
-        examples: [1, 20, 300],
-      }),
-      prerequisites: z
-        .array(z.string())
-        .optional()
-        .meta({
-          description:
-            "The prerequisites of other challenges that must be solved before this challenge",
-          examples: [["challenge-1", "challenge-2"]],
+        slug: z
+          .string()
+          .optional()
+          .meta({
+            description:
+              "The short name for the challenge. Useful when the ID is a UUID.",
+            examples: ["challenge-1", "challenge-2"],
+          }),
+        category: z.string().meta({
+          description: "The category of the challenge",
+          examples: ["web", "crypto", "forensics"],
         }),
-    }),
-  ),
-})
+        value: z.number().meta({
+          description: "The score/value of the challenge",
+          examples: [1, 20, 300],
+        }),
+        prerequisites: z
+          .array(z.string())
+          .optional()
+          .meta({
+            description:
+              "The prerequisites of other challenges that must be solved before this challenge",
+            examples: [["challenge-1", "challenge-2"]],
+          }),
+      }),
+    ),
+  })
   .meta({
     $schema: "https://json-schema.org/draft-07/schema",
     $id: "https://json.schemastore.org/ctf-challenge-pack",
@@ -41,6 +42,6 @@ export const ChallengePack = z.object({
     version: process.env.npm_package_version,
     description:
       "A pack of challenges, including their categories, values, and prerequisites.",
-  });;
+  });
 
 export default ChallengePack;
